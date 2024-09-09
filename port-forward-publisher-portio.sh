8d4c103f03e5d0286317c6835f9c8980e420f528
tamarleviCm <110327792+tamarleviCm@users.noreply.github.com>
2024-09-09 12:31:41 +0300

    Add files via upload

---
+++ port-forward-publisher-portio.sh
@@ -0,0 +1,11 @@
+# Port forward commands using service name prefixes
+kubectl port-forward service/core-core-results 26500 26502 26503 26504 &
+kubectl port-forward service/core-core-scans 26501 28502:26502 28503:26503 &
+kubectl port-forward service/core-core-feature-flag 28500:26500 &
+kubectl port-forward service/integrations-integrations-datastore 5437 &
+kubectl port-forward service/integrations-integrations-feedback-app 5438 &
+kubectl port-forward service/integrations-integrations-repos-manager 5433 &
+kubectl port-forward service/platform-rabbitmq 5672 15672 &
+kubectl port-forward service/platform-postgresql 5432 5434 &
+
+#kubectl port-forward service/integrations-integrations-emailer 4978 &
